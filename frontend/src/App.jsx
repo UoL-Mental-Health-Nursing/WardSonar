a0b8608c2056add196f02b7255af69f5fad456e3
Mysha Zaman <146783271+myshazaman13@users.noreply.github.com>
2025-06-30 10:20:26 +0000

    checking auto deployment 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,15 +8,25 @@ function App() {
 
   return (
     <>
-      <h1>Welcome to <br>WardSonar</br></h1>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>WardSonar</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-  
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
       </div>
       <p className="read-the-docs">
-       Click here to get started
+        Click on the Vite and React logos to learn more
       </p>
     </>
   )
